df562a89a58715feaa73f0bedee3a932d858481c
miss-mad <mmlh.harris@gmail.com>
2022-07-18 23:26:06 -0400

    outline

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-var wins = 0;
-var ties = 0;
-var losses = 0;
+// accept user input for the key up event R, P, S
+
+// btn for playing again
+    // when a user clicks "Play Again" button
+    // runs a function to clear the board/reset the game
+    // confirm() text "Do you want to play again?" 
+        // Yes - reset game
+        // No - Thanks for playing and then navigates to miss-mads portfolio
+        // OR No - then rickroll video YASSS
+
+// some storage for stats
+    // wins 
+    // ties
+    // losses
+    // rounds played
+
 
